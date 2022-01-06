b46fb93682c880fff6e1378acf392f932c02bf57
aimad bachar <aimadbacharpro@outlook.fr>
2022-01-06 17:00:04 +0100

    add event clic on product with alert

--- src/components/ItemProduct/ItemProduct.js
+++ src/components/ItemProduct/ItemProduct.js
@@ -1,8 +1,13 @@
 import './style.scss';
 
+import ModalInfo from '../ModalInfo/ModalInfo';
+
 const ItemProduct = ({product}) => {
+    const handleClick = () => {
+        alert(`Vous avez cliqué sur le produit ${product.name}, Bon choix !`);
+    }
     return (
-        <div className="card">
+        <div className="card" onClick={handleClick}>
             <div className="item-product">
                 <div className="item-product__img">
                     <img src={product.img} alt={product.name} />
@@ -11,7 +16,7 @@ const ItemProduct = ({product}) => {
                     <h3 className="item-product__name">{product.name}</h3>
                     <p className="item-product__price">{product.price} €</p>
                 {product.isSaled && <p className="item-product__sale">Solde</p>} 
-                {product.quantity === 0 && <span className="item-product__out-of-stock">Out of stock</span>}
+                {product.quantity === 0 && <span className="item-product__out-of-stock" style={{color: 'red'}}>Out of stock</span>}
                 </div>
             </div>
         </div>
