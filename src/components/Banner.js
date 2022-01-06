b58766dcaa911c36d432b4398fe5eb73e414e6c3
aimad bachar <aimadbacharpro@outlook.fr>
2022-01-06 10:30:45 +0100

    create component Banner

---
+++ src/components/Banner.js
@@ -0,0 +1,13 @@
+import Description from './Description.js';
+import Header from './Header.js';
+
+const Banner = () => {
+    return (
+        <div className="banner">
+            <Header />
+            <Description />
+        </div>
+    );
+}
+
+export default Banner;
