296b285bba842bd8c692c0f13502be9510810ff8
aimad bachar <aimadbacharpro@outlook.fr>
2022-01-06 11:42:28 +0100

    change place Cart into App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 
 // Components
 import Banner from './components/Banner';
+import Cart from './components/Cart.js';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Cart />
     </div>
   );
 }
