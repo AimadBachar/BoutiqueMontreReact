c6136013c91d07e3529b9ecac6c76589bfef241b
aimad bachar <aimadbacharpro@outlook.fr>
2022-01-06 12:03:13 +0100

    add style directly in the component in html tag

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,7 +1,17 @@
+// import './style.scss';
+
 const Description = () => {
+    const styleSlogan = {
+        color: '#f39',
+        fontSize: '1.5rem',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        margin: '0 auto',
+        padding: '1rem 0'
+    };
     return (
         <div className="description">
-            <h3>Mettez vous à l'heure avec nos magnifiques montres</h3>
+            <h3 style={styleSlogan}>Mettez vous à l'heure avec nos magnifiques montres</h3>
         </div>
     );
 }
