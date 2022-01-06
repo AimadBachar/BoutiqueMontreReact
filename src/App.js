95a7974df37751585fa261bddc0d2f8c3b254517
aimad bachar <aimadbacharpro@outlook.fr>
2022-01-06 10:31:07 +0100

    add component Banner in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 
 // Components
-import Header from './components/Header.js';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Banner />
     </div>
   );
 }
