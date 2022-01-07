a1114ccda031df44faa11fb5e803c2fbcec06dc0
aimad bachar <aimadbacharpro@outlook.fr>
2022-01-07 10:02:36 +0100

    example useState in App

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import bannerImg from './assets/images/banner.jpg';
 // Components
 import Banner from './components/Banner/Banner.js';
 // import Cart from './components/Cart/Cart.js';
-
 import ShoppingList from './components/ShoppingList/ShoppingList';
 
 // data
 import products from './data/productList.js';
 
+// import { useState } from 'react';
+import Clock from './components/Clock/Clock';
 
 function App() {
+  // let [count, setCount] = useState(0);
+  
+
   return (
     <div className="App">
       <img src={bannerImg} alt="banner-clock" className="banner-img"/>
       <Banner />
+      {/* <p>{ count }</p>
+      <button onClick={()=>setCount(count++)}>Increment</button> */}
       {/* <Cart /> */}
+      <Clock />
       <ShoppingList products={products}/>
     </div>
   );
