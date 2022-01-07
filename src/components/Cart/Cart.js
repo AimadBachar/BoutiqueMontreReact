9300ffdb3683453592591a47d25541b5424803b4
aimad bachar <aimadbacharpro@outlook.fr>
2022-01-07 14:27:05 +0100

    add button add,clear,open cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,34 +1,78 @@
+import { useState } from 'react';
 import './style.scss';
 
+// FUNCTIONS
+function getIdValide(products)
+{
+    let id = 0;
+    products.forEach((element) => {
+        if(element.id > id)
+        {
+            id = element.id;
+        }
+    });
+    return id + 1;
+}
 const Cart = () => {
-    const products = [
+    // Fake data
+    const products0 = [
         {id: 1, name: 'Montre Dior', price: 1300 },
         {id: 2, name: 'Montre Tommy', price: 2700}, 
         {id: 3, name: 'Montre Rolex', price: 4000}
     ];
 
-    const list = products.map(product => {
-        return <li className='cart-items__item' key={product.id}>{product.name}: {product.price}</li>
-    });
+    // STATES
+    const [products, setProduct] = useState(products0);
+    const [isOpen, setIsOpen] = useState(false);
 
-    const total = products.reduce((tot, product) => {
-        return tot + product.price;
-    }, 0);
+    const [list, setList] = useState(
+        products.map((product) => (
+            <li className='cart-items__item' key={product.id}>{product.name}: {product.price}</li>
+        ))
+    );
+        
+    // FUNCTIONS
+    let Sum = 0;
+  products.forEach((element) => (Sum += element.price));
 
+
+  if (isOpen) {
     return (
         <div className="cart">
-            <h3>Votre panier</h3>
-            <div className="cart-group">
-                <ul className="cart-items">
-                    {list}
-                </ul>
-            </div>
-            <div className="cart-total">
-                <h4>Total: </h4>
-                <h4>{ total } €</h4>
-            </div>
+            <button className="cart-button" onClick={() => setIsOpen(!isOpen)}>Cacher le panier</button>
+          <h4>Votre panier</h4>
+          <ul>{list}</ul>
+          <div> Total:{Sum}</div>
+          <button
+            onClick={() => {
+              products.push({
+                id: getIdValide(products),
+                name: "Montre Mickey",
+                price: 10
+              });
+              setProduct(products);
+              setList(
+                products.map((element) => (
+                  <li key={element.id}>
+                    {element.name} {element.price} Eur
+                  </li>
+                ))
+              );
+            }}
+          >
+            Add product
+          </button>
+          <button onClick={() =>{
+              setProduct([]);
+                setList([]);
+          } }>Clear cart</button>
         </div>
     )
+  }
+  
+  return (
+      <button onClick={()=>setIsOpen(true)}>Afficher le panier</button>
+  )
 }
 
 export default Cart;
